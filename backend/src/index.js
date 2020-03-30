76002f1dec058eddede8984d98cc26b2d6e12c72
Leonardo Gomes Batista <leonardog.bat@gmail.com>
2020-03-30 18:13:26 -0300

    Add ReachJS frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,35 +1,3 @@
-/** 
- * Rota / Recurso
- */
-
-/**
- * Métodos HTTP:
- * 
- * GET: Buscar/listar uma informação do back-end
- * POST: Criar uma informação no back-end
- * PUT: Alterar uma informação no back-end
- * DELETE: Deletar uma informação no back-end
- * 
- */
-
-/**
- * Tipos de parâmetros
- * 
- * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
- *           Ex: http://localhost:3333/users?name=Leonardo
- * Route Params: Parâmetros utilizados para identificar recursos
- * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
- */
-
-/**
- * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server, ...
- * NoSQL: MongoDB, CouchDB, ...
- */
-
- /**
-  * Driver: SELECT * FROM users
-  * Query Builder: table('users').select('*').where(...)
-  */
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
